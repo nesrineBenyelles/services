6447c76fd715a53743bd7f665b8ca9f1f5b8f3b1
nesrineBenyelles <121671098+nesrineBenyelles@users.noreply.github.com>
2023-04-12 03:01:58 +0100

    Add files via upload

---
+++ Services.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import justice from './justice.jpg'
+import famille from './family.png'
+import livrededroit from './livre-de-droit.png'
+import invesment from './invesment.png'
+import trademark from './trademark.png'
+import handcuffs from './handcuffs.png'
+import droitdessocietes from './droit-des-societes.png'
+
+
+export const Services = () => {
+  return (
+    <body>
+    <img className='backgroundimg' src={justice} ></img>
+    
+  <div class="service">
+    <img src={livrededroit} alt="Service 1"></img>
+    <h3>Avocat généraliste</h3>
+    <p>Contrairement aux avocats qui se spécialisent dans un domaine particulier du droit, un avocat généraliste a une pratique qui traite un large éventail de questions juridiques. Différents avocats de pratiques générales auront différents domaines de droit avec lesquels ils sont les plus à l’aise, donc si vous consultez un avocat généraliste, il est toujours prudent de discuter de son expérience dans la gestion du type de problème juridique que vous rencontrez.<br>
+    </br>
+    Il existe de nombreux types d’avocats disponibles sur le marché légal aujourd’hui. Lorsque vous cherchez de l’aide juridique pour régler un problème auquel vous êtes confronté, c’est une bonne idée de trouver un avocat qui a de l’expérience dans le domaine juridique précis avec lequel vous traitez.
+    </p>
+  </div>
+
+  <div class="service">
+    <img src={famille} alt="Service 2"></img>
+    <h3>Avocat familial</h3>
+    <p>Que vous ayez engagé dans une procédure de divorce ou impliqué dans une garde d’enfants ou une pension alimentaire pour époux, un avocat spécialisé en droit de la famille est le mieux placé pour vous guider dans le processus qui se déroule devant vous.</p>
+  </div>
+
+  <div class="service">
+    <img src={handcuffs} alt="Service 3"></img>
+    <h3>Avocat de droit pénal</h3>
+    <p>Un avocat de droit pénal est un professionnel du droit qui représente des personnes accusées d'avoir commis des crimes. Les avocats de droit pénal peuvent être appelés à intervenir à différents stades du processus judiciaire, depuis l'enquête préliminaire jusqu'au procès en passant par la sentence. Leur rôle est de protéger les droits de leurs clients, notamment leur droit à une défense équitable et leur droit à la présomption d'innocence.<br></br>
+
+Les avocats de droit pénal travaillent souvent en étroite collaboration avec leurs clients pour comprendre les circonstances de l'infraction alléguée et pour préparer une défense solide. Ils peuvent également aider leurs clients à négocier des accords de plaidoyer, à obtenir une libération sous caution ou à faire appel d'une condamnation. Enfin, les avocats de droit pénal sont également des experts en matière de droit pénal, de procédures judiciaires et de preuves, et sont en mesure de conseiller leurs clients sur les conséquences juridiques de leurs actions.<br></br>
+
+Dans le cadre de leur travail, les avocats de droit pénal doivent faire preuve de rigueur, d'intégrité et de compétence pour assurer une défense efficace de leurs clients. Ils doivent également être en mesure de communiquer clairement avec leurs clients, de travailler sous pression et de prendre des décisions éclairées en temps opportun.</p>
+  </div>
+  <div class="service">
+    <img src={invesment} alt="Service 4"></img>
+    <h3>Avocat de droit immobilier</h3>
+    <p>Un avocat spécialisé en droit immobilier est un professionnel juridique qui fournit des conseils juridiques aux personnes impliquées dans des transactions immobilières ou des litiges immobiliers. Les avocats de droit immobilier peuvent représenter des propriétaires fonciers, des promoteurs immobiliers, des investisseurs, des locataires et d'autres parties prenantes dans des transactions immobilières complexes. Ils peuvent également aider leurs clients à résoudre des différends, tels que des litiges de propriété, des problèmes de zonage et de construction, des poursuites en matière de baux, des questions de financement et d'autres problèmes juridiques liés à l'immobilier. Les avocats de droit immobilier ont une connaissance approfondie des lois et des règlements applicables à l'achat, la vente et la location de biens immobiliers et travaillent en étroite collaboration avec leurs clients pour protéger leurs intérêts et atteindre leurs objectifs commerciaux.</p>
+  </div>
+  <div class="service">
+    <img src={trademark} alt="Service 3"></img>
+    <h3>Avocat de Droit de la propriété intellectuelle</h3>
+    <p>Un avocat de droit de propriété intellectuelle est un professionnel du droit spécialisé dans les questions liées à la propriété intellectuelle, qui englobe notamment les brevets, les marques de commerce, les droits d'auteur et les secrets commerciaux. Leur rôle est de conseiller et représenter les clients dans toutes les questions liées à la propriété intellectuelle, notamment l'enregistrement des droits de propriété intellectuelle, la négociation de licences et de contrats, la protection des marques de commerce, la défense des droits d'auteur et la poursuite en justice en cas de violation de la propriété intellectuelle.<br></br>
+    Les avocats de droit de propriété intellectuelle doivent être en mesure de comprendre les lois et les réglementations complexes qui régissent la propriété intellectuelle, ainsi que les aspects techniques et scientifiques qui sont souvent impliqués. Ils doivent également être en mesure d'analyser les risques juridiques et commerciaux liés aux questions de propriété intellectuelle et de fournir des conseils stratégiques à leurs clients.<br></br>
+    En travaillant avec un avocat de droit de propriété intellectuelle, les clients peuvent s'assurer que leurs droits de propriété intellectuelle sont protégés et que leur entreprise est en mesure d'exploiter pleinement la valeur de leurs actifs de propriété intellectuelle. Les avocats de droit de propriété intellectuelle peuvent également aider à négocier des accords de licence ou de transfert de propriété intellectuelle, à résoudre des litiges relatifs à la propriété intellectuelle et à défendre les intérêts de leurs clients devant les tribunaux et les organismes de réglementation compétents.</p>
+  </div>
+
+  <div class="service">
+    <img src={droitdessocietes} alt="Service 3"></img>
+    <h3>Avocat de Droit de la propriété intellectuelle</h3>
+    <p>Un avocat spécialisé en droit des affaires est un professionnel juridique qui conseille les entreprises et les entrepreneurs sur les questions juridiques liées aux activités commerciales. Les avocats de droit des affaires peuvent travailler sur une variété de domaines, y compris la formation de sociétés, la négociation de contrats commerciaux, la propriété intellectuelle, la conformité réglementaire, les fusions et acquisitions, les litiges commerciaux et d'autres aspects liés à la gestion des entreprises. Les avocats de droit des affaires travaillent en étroite collaboration avec leurs clients pour comprendre les objectifs commerciaux et les aider à naviguer dans les complexités juridiques des transactions commerciales. Ils peuvent également aider à prévenir les litiges en élaborant des stratégies juridiques proactives pour résoudre les problèmes avant qu'ils ne deviennent des problèmes juridiques importants. Les avocats de droit des affaires sont des conseillers juridiques essentiels pour toute entreprise, qu'elle soit grande ou petite, et sont indispensables pour aider les entreprises à atteindre leurs objectifs commerciaux tout en restant en conformité avec les lois et les réglementations applicables.</p>
+  </div>
+    </body>
+    )
+}
